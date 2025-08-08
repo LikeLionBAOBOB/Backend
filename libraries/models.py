7864ff9824a455ba22791241cfd047c7419e11d8
0euun <jeyejong70@naver.com>
2025-08-08 21:16:19 +0900

    ✨ feat: libraries 앱 모델 작성

--- libraries/models.py
+++ libraries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Library(models.Model):
+    title = models.CharField(max_length=30, null=False)  # 도서관 이름
+    seats_all = models.IntegerField(null=False)  # 전체 좌석 수
+    is_closed = models.BooleanField(null=False)  # 운영 여부
+    worktime = models.TextField(null=False)  # 운영시간
+    map_url = models.TextField(null=False)  # 네이버 지도 연동 링크
+    service_text = models.CharField(max_length=50, null=False)  # 편의시설 및 서비스
+    address = models.CharField(max_length=50, null=False)  # 주소
+    latitude = models.FloatField(null=False)  # 위도
+    longitude = models.FloatField(null=False)  # 경도
+    image = models.URLField(null=False)  # 도서관 이미지
+    library_link = models.TextField(null=False)  # 도서관 링크
+
+    def __str__(self):
+        return self.title
