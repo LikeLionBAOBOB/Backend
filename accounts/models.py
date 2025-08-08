13cb2d731e1e701de1ce9b6d6b6b08e5f7d545cf
0euun <jeyejong70@naver.com>
2025-08-08 21:11:55 +0900

    � docs: accounts 앱 모델 일단 푸시...

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+# abstractuser 상속 받지 않고 그냥 모델 두 개로.... -> 가능할지...?
+# class User(models.Model):
+#     username = models.CharField(max_length=150, unique=True, null=True, blank=True)
+#     phone = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return self.username
+
+
+# class Manager(models.Model):
+#     username = models.CharField(max_length=150, unique=True, null=True, blank=True)
+#     email = models.EmailField(unique=True, null=True, blank=True)
+#     password = models.CharField(max_length=128)
+
+#     def __str__(self):
+#         return self.username
