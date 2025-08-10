0f7e23b13655d5ef2dbcac3cdc6ed96c952608d6
0euun <jeyejong70@naver.com>
2025-08-10 19:26:06 +0900

    ✨ feat: Library 모델 수정

--- libraries/models.py
+++ libraries/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Library(models.Model):
+    ''' 해당 값들은 API에서 불러올 거기 때문에 주석 처리
     title = models.CharField(max_length=30, null=False)  # 도서관 이름
     seats_all = models.IntegerField(null=False)  # 전체 좌석 수
     is_closed = models.BooleanField(null=False)  # 운영 여부
@@ -13,9 +14,12 @@ class Library(models.Model):
     longitude = models.FloatField(null=False)  # 경도
     image = models.URLField(null=False)  # 도서관 이미지
     library_link = models.TextField(null=False)  # 도서관 링크
+    '''
+
+    lib_code = models.IntegerField(unique=True, db_index=True)
 
     def __str__(self):
-        return self.title
+        return str(self.lib_code)
 
 
 class UserPin(models.Model):
@@ -26,4 +30,4 @@ class UserPin(models.Model):
         unique_together = ('user', 'library')
 
     def __str__(self):
-        return f"{self.user.nickname} - {self.library.title}"
+        return f"{self.user.nickname} - {self.library.lib_code}"
