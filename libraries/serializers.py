8761b5ebcde3bf773f2185bfdf189ec0d954bbce
0euun <jeyejong70@naver.com>
2025-08-10 17:49:27 +0900

    ✨ feat: 도서관 정보 확인 (간략) 기능 추가

---
+++ libraries/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from django.conf import settings
+import json
+from pathlib import Path
+
+def load_library_info():
+    data_path = Path(settings.BASE_DIR) / "libraries" / "data" / "library_info.json"
+    with open(data_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+LIBRARY_INFO = load_library_info()
+
+
+# 도서관 정보 확인 (간략)
+class SimpleLibrarySerializer(serializers.Serializer):
+    name = serializers.CharField()
+    image = serializers.CharField()
+    current_seats = serializers.IntegerField()
+    total_seats = serializers.IntegerField()
+    congestion = serializers.CharField()
+    is_open = serializers.CharField()
+    operating_time = serializers.CharField()
+
+    def to_representation(self, instance):
+        lib, lib_code = instance
+        current_hours = LIBRARY_INFO.get(str(lib_code), {}).get("current_hours", "")
+
+        return {
+            "name": lib.get("libName"),
+            "image": f"{settings.MEDIA_URL}libraries/{lib_code}.png",  # lib_code는 views.py 참고
+            "current_seats": 0,      # 실제 좌석 수 반영 -> 추후 수정 필요
+            "total_seats": 0,        # 실제 좌석 수 반영 -> 추후 수정 필요
+            "congestion": "보통",    # 혼잡도 계산 -> 추후 수정 필요
+            "is_open": "운영 중",    # 현재 시간 2시로 고정
+            "operating_time": current_hours
+        }
--- libraries/urls.py
+++ libraries/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = 'libraries'
 
 urlpatterns = [
+    path('<int:lib_code>/simple/', LibrarySimpleView.as_view(), name='simple-library'),  # /libraries/<int: library_id>/simple/ -> library_id에 lib_code 입력하면 됨
 ]
