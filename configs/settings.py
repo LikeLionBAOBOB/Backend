31e9785fb085eb1961619fbc30ebca2230e31c8d
0euun <jeyejong70@naver.com>
2025-08-15 22:02:45 +0900

    ⚙️  chore: CORS 설정

--- configs/settings.py
+++ configs/settings.py
@@ -75,6 +75,29 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'configs.urls'
 
 TEMPLATES = [
